46b9b9a3e630834d2a66b4312f64b70244f6fecf
cdsorge04 <cdsorge04@gmail.com>
2026-03-31 19:37:46 -0500

    part 7 done

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var sandwichrouter = require('./routes/sandwich');
-
+var gridRouter = require('./routes/grid');
 var app = express();
 
 // view engine setup
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/sandwich', sandwichrouter);
-
+app.use('/grid', gridRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
