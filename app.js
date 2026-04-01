821c01e264fae1527a9c8f53977f79e1f449dace
cdsorge04 <cdsorge04@gmail.com>
2026-03-31 20:50:30 -0500

    done

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var sandwichrouter = require('./routes/sandwich');
 var gridRouter = require('./routes/grid');
+var pickRouter = require('./routes/pick');
 var app = express();
 
 // view engine setup
@@ -24,6 +25,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/sandwich', sandwichrouter);
 app.use('/grid', gridRouter);
+app.use('/selector', pickRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
